a72d237bd796be4b798eeed350d4bfcb8fc38036
RomanKosmyna <roma.kosmyna@gmail.com>
2024-05-02 01:05:48 +0300

    Add preparation & testing instruction steps in Instruction component (fixed)

--- src/components/Layout/GeneralLayout.tsx
+++ src/components/Layout/GeneralLayout.tsx
@@ -4,7 +4,7 @@ type GeneralLayoutProps = {
 
 export default function GeneralLayout({ children }: GeneralLayoutProps) {
     return (
-        <div className="w-full max-w-[1150px] min-h-[calc(100vh-64px)] flex flex-col px-4">
+        <div className="w-full max-w-[1150px] min-h-[calc(100vh-64px)] flex flex-col px-4 pb-10">
             {children}
         </div>
     )
--- src/features/home/components/Instruction.tsx
+++ src/features/home/components/Instruction.tsx
@@ -1,11 +1,14 @@
 import testCategoriesImage from "../assets/test-categories-instruction-step.png";
 import testsByCategoryImage from "../assets/tests-by-category-instruction-step.png";
+import specificTestPreparationImage from "../assets/specific-test-preparation-instruction-step.png";
+import specificTestTestingImage from "../assets/specific-test-testing-instruction-step.png";
+import { Link } from "react-router-dom";
 
 export default function Instruction() {
     return (
-        <div className="w-full pt-10">
+        <div className="w-full pt-10 flex flex-col">
             <ol className="w-full flex flex-col gap-3">
-                <li className="max-w-[600px] max-h-[355px] bg-main shadow-borderLight p-4 rounded-lg">
+                <li className="max-w-[600px] max-h-[355px] bg-main shadow-instructionCards p-4 rounded-lg">
                     <div>
                         <h3 className="font-bold text-xl">Choose a Category of Interest for Assessment.</h3>
                         <p className="mt-2 text-[#171717]">Each category represents a curated set of tests categorized by programming languages.</p>
@@ -16,10 +19,10 @@ export default function Instruction() {
                         className="max-w-full h-[200px] object-contain rounded-lg mt-3"
                     />
                 </li>
-                <li className="max-w-[600px] max-h-[355px] bg-main shadow-borderLight p-4 rounded-lg">
+                <li className="max-w-[600px] max-h-[355px] bg-main shadow-instructionCards p-4 rounded-lg">
                     <div>
                         <h3 className="font-bold text-xl">Select a Specific Test.</h3>
-                        <p className="mt-2 text-[#171717]">Upon selecting a specific category, you will receive a list of tests varying in complexity.</p>
+                        <p className="mt-2 text-[#171717] text-justify">Upon selecting a specific category, you will receive a list of tests varying in complexity.</p>
                     </div>
                     <img
                         src={testsByCategoryImage}
@@ -27,7 +30,40 @@ export default function Instruction() {
                         className="max-w-full h-[200px] object-contain rounded-lg mt-3"
                     />
                 </li>
+                <li className="max-w-[600px] max-h-[355px] bg-main shadow-instructionCards p-4 rounded-lg">
+                    <div>
+                        <h3 className="font-bold text-xl">Prepare for the Test.</h3>
+                        <p className="mt-2 text-[#171717] text-justify">Here you can get more detailed information about a specific test.</p>
+                        <p className="mt-2 text-[#171717] text-justify">When you are ready, press the Start button.</p>
+                    </div>
+                    <img
+                        src={specificTestPreparationImage}
+                        alt=""
+                        className="max-w-full h-[200px] object-contain rounded-lg mt-3"
+                    />
+                </li>
+                <li className="max-w-[600px] max-h-[355px] bg-main shadow-instructionCards p-4 rounded-lg">
+                    <div>
+                        <h3 className="font-bold text-xl">Run the Test.</h3>
+                        <p className="mt-2 text-[#171717] text-justify">Try to answer as many questions as you can.</p>
+                        <p className="mt-2 text-[#171717] text-justify">When you are done, press the Finish Test button and you will get the results.</p>
+                    </div>
+                    <img
+                        src={specificTestTestingImage}
+                        alt=""
+                        className="max-w-full h-[200px] object-contain rounded-lg mt-3"
+                    />
+                </li>
             </ol>
+            <div className="w-full flex justify-center">
+                <Link
+                    className="bg-lime-600 px-6 py-2 mt-10 font-bold text-xl 
+                    text-main flex justify-center rounded-lg transition-colors hover:bg-[#0A6847]"
+                    to={"/test-categories"}
+                >
+                    Go to Tests
+                </Link>
+            </div>
         </div>
     );
 }
