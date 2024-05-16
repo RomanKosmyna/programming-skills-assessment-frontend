8450e81407706671012aa67e98b72fe1de723beb
RomanKosmyna <roma.kosmyna@gmail.com>
2024-05-17 01:50:13 +0300

    Update request endpoints in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const API_URL = "https://localhost:7186/api" as string;
 
 const account = "/account";
-const testTypes = "/testcategory";
+const testCategory = "/testcategory";
 const tests = "/test";
 const testResult = "/testresult";
 const userTestResult = "/usertestresult";
@@ -13,21 +13,21 @@ export const URLS = {
         getUserId: `${account}/getuserid` 
     },
     testCategories: {
-        getAll: `${testTypes}/getalltestcategories`
+        getAll: `${testCategory}/testcategories`
     },
     testsByCategory: {
-        getAll: `${tests}/getAllTests`,
-        getById: (testId: string | undefined) => `${tests}/gettestbyid/${testId}`,
-        getByIdWithRelatedTables: (testId: string | undefined) => `${tests}/gettestbyidwithrelatedtables/${testId}`,
-        getByTestCategoryId: (testTypeId: string | undefined) => `${tests}/gettestsbytestcategoryid/${testTypeId}`
+        getAll: `${tests}/tests`,
+        getById: (testId: string | undefined) => `${tests}/test/${testId}`,
+        getByIdWithRelatedTables: (testId: string | undefined) => `${tests}/testwithrelateddata/${testId}`,
+        getByTestCategoryId: (testTypeId: string | undefined) => `${tests}/testsbycategory/${testTypeId}`
     },
     testResult: {
         formTestResult: (testId: string | undefined) => `${testResult}/formtestresult/${testId}`
     },
     userTestResult: {
-        saveUserTestResult: `${userTestResult}/savetestresult`,
-        getAllUserTestResults: (username: string | undefined) => `${userTestResult}/getallusertestresults/${username}`,
-        getUserTestResultById: (userTestResultId: string | undefined) => `${userTestResult}/getusertestresultbyid/${userTestResultId}`,
-        deleteUserTestResultById: (userTestResultId: string | undefined) => `${userTestResult}/deleteusertestresult/${userTestResultId}`
+        saveUserTestResult: `${userTestResult}/testresult`,
+        getAllUserTestResults: (username: string | undefined) => `${userTestResult}/testresults/${username}`,
+        getUserTestResultById: (userTestResultId: string | undefined) => `${userTestResult}/testresult/${userTestResultId}`,
+        deleteUserTestResultById: (userTestResultId: string | undefined) => `${userTestResult}/testresult/${userTestResultId}`
     }
 };
